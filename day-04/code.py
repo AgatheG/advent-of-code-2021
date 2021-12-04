c6819af6e80e531b1e75b45c9f833f0032aceef9
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-12-04 08:57:16 +0100

    Do both parts in same loop + Cleaning of Board class

--- day-04/code.py
+++ day-04/code.py
@@ -6,54 +6,41 @@ class Board(object):
     NO_WIN = 0
 
     def __init__(self, grid):
-        self.values = grid.copy()
-        self.cols = [0]*self.BOARD_SIDE
-        self.rows = [0]*self.BOARD_SIDE
+        self.values = grid
 
     def check(self, value):
         i, j = np.where(self.values == value)
+        if not i.size: #  Value is not in the board
+            return self.NO_WIN
+
         self.values[i, j] = self.MARKED
 
-        if self.values[:,j].sum() == self.MARKED*self.BOARD_SIDE:
-            return self.values[self.values != self.MARKED].sum() * value
-        if self.values[i,:].sum() == self.MARKED*self.BOARD_SIDE:
+        if self.values[:, j].sum() == self.MARKED * self.BOARD_SIDE or\
+            self.values[i, :].sum() == self.MARKED * self.BOARD_SIDE:
             return self.values[self.values != self.MARKED].sum() * value
         return self.NO_WIN
 
 
+boards = set()
 with open("input.txt", "r") as file:
     drawn_numbers, *grids = file.read().split("\n\n")
     drawn_numbers = np.fromstring(drawn_numbers, dtype=int, sep=',')
-    for i, grid in enumerate(grids):
+    for grid in grids:
         grid = grid.split('\n')
-        grids[i] = np.array([row.lstrip().split(" ") for row in grid], dtype=int)
+        grid = np.array([row.lstrip().split(" ") for row in grid], dtype=int)
+        boards.add(Board(grid))
 
-
-# PART 1
-boards = set(Board(grid) for grid in grids)
-for nr in drawn_numbers:
+# PARTS 1 & 2
+winners = set()
+for number in drawn_numbers:
     for board in boards:
-        score = board.check(nr)
-        winning = score != Board.NO_WIN
-        if winning:
-            break
-    if winning:
-            break
-print("PART 1: " + str(score))
-
-# PART 2
-boards, winners = set(Board(grid) for grid in grids), set()
-for nr in drawn_numbers:
-    for board in boards:
-        score = board.check(nr)
-        winning = score != Board.NO_WIN
-        if winning:
-            if len(boards) == 1:
-                break
-            else:
-                winners.add(board)
-                winning = False
-    if winning:
-        break
+        score = board.check(number)
+        if score != Board.NO_WIN:
+            if not winners:
+                print("Part 1: The first board to win has a score of {}".format(score))
+            winners.add(board)
     boards -= winners
-print("PART 2: " + str(score))
+    if not boards:
+        break
+
+print("Part 2: The last board to win has a score of {}".format(score))
