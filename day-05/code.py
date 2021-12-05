93f6e35ac0e10545724dadf6766c949d0de2ba1f
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-12-05 13:52:56 +0100

    Day 05 but cleaner

--- day-05/code.py
+++ day-05/code.py
@@ -7,32 +7,44 @@ parser = ArgumentParser()
 parser.add_argument("-i", "-input", default="input.txt", dest="file")
 args = parser.parse_args()
 
-segments = []
-N = 0
+ARROW_SEPARATOR = ' -> '
+
+board_side, segments = 0, []
 with open(args.file, "r") as file:
     lines = file.read().split("\n")
     for line in lines:
-        line = line.split(' -> ')
-        x1, y1, x2, y2 = *line[0].split(','), *line[1].split(',')
-        segments.append([int(x1), int(y1), int(x2), int(y2)])
-        N = max(N, int(x1), int(x2))
+        line = line.split(ARROW_SEPARATOR)
+        beginning = np.fromstring(line[0], dtype=int, sep=",")
+        end = np.fromstring(line[1], dtype=int, sep=",")
+        segments.append([*beginning, *end])
+        board_side = max(board_side, *beginning, *end) + 1
 
-A = np.zeros((N+1,N+1))
-B = np.zeros((N+1,N+1))
+without_diags = np.zeros((board_side, board_side))
+diagonals = np.zeros((board_side, board_side))
+
 for x1, y1, x2, y2 in segments:
-    if x1==x2:
-        i1, i2 = min(y1, y2), max(y1, y2)
-        A[i1:i2+1,x1] += 1
-        B[i1:i2+1,x1] += 1
-    elif y1==y2:
-        j1, j2 = min(x1, x2), max(x1, x2)
-        A[y1,j1:j2+1] += 1
-        B[y1,j1:j2+1] += 1
-    else:
-        i1, i2 = min(y1, y2), max(y1, y2)
-        j1, j2 = min(x1, x2), max(x1, x2)
-        size = i2 - i1 + 1
-        is_left_to_right = (x2 < x1 and y2 < y1) or (y1 < y2 and x1 < x2)
-        c = np.eye(size) if is_left_to_right else np.fliplr(np.eye(size))
-        B[i1:i2+1, j1:j2+1] += c
-print(B[B>1].size, A[A>1].size)
+    if x1 == x2:  # VERTICAL LINES
+        first_row, col = min(y1, y2), x1
+        length = abs(y1 - y2) + 1
+        without_diags[first_row : first_row + length, col] += 1
+
+    elif y1 == y2:  # HORIZONTAL LINES
+        row, first_col = y1, min(x1, x2)
+        length = abs(x1 - x2) + 1
+        without_diags[row, first_col : first_col + length] += 1
+
+    else:  # DIAGONALS
+        length = abs(y1 - y2) + 1
+        first_row, first_col = min(y1, y2), min(x1, x2)
+        last_row, last_col = first_row + length, first_col + length
+
+        is_top_left_to_bot_right = (y2 - y1) / (x2 - x1) > 0
+        if is_top_left_to_bot_right:
+            diagonals[first_row : last_row, first_col : last_col] += np.eye(length)
+        else:
+            diagonals[first_row : last_row, first_col : last_col] += np.fliplr(np.eye(length))
+
+print("Part 1", without_diags[without_diags > 1].size)
+
+with_diags = diagonals + without_diags
+print("Part 2", with_diags[with_diags > 1].size)
