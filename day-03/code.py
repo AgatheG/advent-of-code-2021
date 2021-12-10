37f0211a95e747edaaee10ef7ca1fd4f318ec1bd
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-12-10 07:31:49 +0100

    Use a smarter way to convert to decimal (day 03)

--- day-03/code.py
+++ day-03/code.py
@@ -3,16 +3,20 @@ with open("input.txt", "r") as file:
     bit_numbers = file.read().split("\n")
     REPORT = np.array([list(b) for b in bit_numbers], dtype=int)
 
-bits = np.array([2**i for i in range(REPORT.shape[1]-1, -1, -1)])
+#bits = np.array([2**i for i in range(REPORT.shape[1]-1, -1, -1)])
+#def convert_to_decimal(array):
+#    return (bits*array).sum()
+
+# Smarter conversion?
 def convert_to_decimal(array):
-    return (bits*array).sum()
+    return int("".join(array.astype(str)), 2)
 
 def most_common_bit(array):
     length = array.shape[0]
     return array.sum(axis=0) // (length/2)
 
 # PART 1
-most_common_bits = most_common_bit(REPORT)
+most_common_bits = most_common_bit(REPORT).astype(int)
 gamma, eps = convert_to_decimal(most_common_bits), convert_to_decimal(1 - most_common_bits)
 print("PART 1 : The power consumption is {:.0f}".format(gamma * eps))
 
