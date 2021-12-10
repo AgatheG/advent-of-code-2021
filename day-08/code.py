834930ec9bb2f00fa33177f580a0e049cf510b89
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-12-10 07:46:12 +0100

    Day 08: clean up

--- day-08/code.py
+++ day-08/code.py
@@ -13,36 +13,34 @@ with open(args.file, "r") as file:
         signal, output = line.split(' | ')
         lines[idx] = [signal.split(" "), output.split(" ")]
 
-S = 0
-for signal, output in lines:
+easy_nr_occurrences = 0
+for _, output in lines:
     for digit in output:
-        if len(digit) == 2 or len(digit) == 3 or len(digit) == 4 or len(digit)==7:
-            S +=1
-print(S)
+        if len(digit) in {2, 3, 4, 7}:
+            easy_nr_occurrences += 1
+print("PART 1: The 'easy' numbers appear {} times".format(easy_nr_occurrences))
 
-D = {
-    0: {"a", "b", "c", "e", "f", "g"},
-    1: {"c", "f"},
-    2: {"a", "c", "d", "e", "g"},
-    3: {"a", "c", "d", "f", "g"},
-    4: {"b", "c", "d", "f"},
-    5: {"a", "b", "d", "f", "g"},
-    6: {"a", "b", "d", "e", "f", "g"},
-    7: {"a", "c", "f"},
-    8: {"a", "b", "c", "d", "e", "f", "g"},
-    9: {"a", "b", "c", "d", "f", "g"}
+SEGMENTS = 'abcdefg'
+
+SEGMENTS_TO_DIGIT = {
+    'abcefg': '0',
+    'cf': '1',
+    'acdeg': '2',
+    'acdfg': '3',
+    'bcdf': '4',
+    'abdfg': '5',
+    'abdefg': '6',
+    'acf': '7',
+    'abcdefg': '8',
+    'abcdfg': '9'
 }
 
-I = {
-    "".join(sorted(v)): str(k) for k, v in D.items()
-}
-K = {
-    2: (1,),
-    3: (7,),
-    4: (4,),
-    5: (2, 3, 5),
-    6: (0, 6, 9),
-    7: (8,),
+POTENTIAL_SEGMENTS = {
+    2: dict(union={'c', 'f'}, intersection={'c', 'f'}),
+    3: dict(union={'a', 'c', 'f'}, intersection={'a', 'c', 'f'}),
+    4: dict(union={'b', 'c', 'd', 'f'}, intersection={'b', 'c', 'd', 'f'}),
+    5: dict(union=set(list(SEGMENTS)), intersection={'a', 'd', 'g'}),
+    6: dict(union=set(list(SEGMENTS)), intersection={'a', 'b','f', 'g'})
 }
 
 def replace(mapping, string):
@@ -51,30 +49,31 @@ def replace(mapping, string):
         characters += mapping[s]
     return "".join(sorted(characters))
 
-SS = 0
+sum_output_values = 0
 for signal, output in lines:
-    mapping = {
-        l: set(list('abcdefg')) for l in 'abcdefg'
-    }
+    mapping = { l: set(list(SEGMENTS)) for l in SEGMENTS }
     found = set()
     for letters in [*signal, *output]:
-        potentials, qotentials = set(), set(list('abcdefg'))
-        for d in K[len(letters)]:
-            potentials |= D[d]
-            qotentials &= D[d]
-        s = set()
+        potential_segments = POTENTIAL_SEGMENTS.get(len(letters), None)
+        if potential_segments is None:
+            # case of digit 8: useless
+            continue
+
+        union, intersection = potential_segments.values()
         for key, charset in mapping.items():
             if len(charset) == 1:
                 continue
             charset -= found
             if key in letters:
-                charset &= potentials
+                charset &= union
             else:
-                charset -= qotentials
+                charset -= intersection
             if len(charset) == 1:
                 found |= charset
+        
     number = ''
     for digit in output:
-        number += I[replace(mapping, digit)]
-    SS += int(number)
-print(SS)
+        number += SEGMENTS_TO_DIGIT[replace(mapping, digit)]
+    sum_output_values += int(number)
+
+print("PART 2: The sum of the 4 digit numbers is {}".format(sum_output_values))
